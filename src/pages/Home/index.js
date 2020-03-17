b6c0fa5ff52854f54149265117897bd7e6dd8212
ARTHUR PC <arthurpimetelcordeiro@gmail.com>
2020-03-17 14:49:14 -0300

    Refatorando as actions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,9 +2,12 @@
 /* eslint-disable react/prop-types */
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { MdAddShoppingCart } from 'react-icons/md'
 import { formatPrice } from '../../util/format'
 import api from '../../services/api'
+
+import * as CartActions from '../../store/modules/cart/actions'
 
 import { ProductList } from './styles'
 
@@ -25,12 +28,9 @@ class Home extends Component {
   }
 
   handleAddProduct = product => {
-    const { dispatch } = this.props
+    const { addToCart } = this.props
 
-    dispatch({
-      type: 'ADD_TO_CART',
-      product,
-    })
+    addToCart(product)
   }
 
   render() {
@@ -61,4 +61,6 @@ class Home extends Component {
   }
 }
 
-export default connect()(Home)
+const mapDispatchToProps = dispatch => bindActionCreators(CartActions, dispatch)
+
+export default connect(null, mapDispatchToProps)(Home)
