90d2bb1633f8848cfbc11646ea9e9046fe9206ab
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-03-30 09:17:58 -0300

    Update base URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://my-json-server.typicode.com/arthurpc03/api-rocketshoes',
 })
 
 export default api
