9c63e30f6760d02b44adbbdebef2f2abc10088f1
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-03-30 14:34:55 -0300

    json-server heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/arthurpc03/api-rocketshoes',
+  baseURL: 'https://my-json-server-api.herokuapp.com',
 })
 
 export default api
 
 // https://my-json-server.typicode.com/arthurpc03/api-rocketshoes
 // http://localhost:3333
+// https://my-json-server-api.herokuapp.com
